3afa010bf6150d4ea1ea598c104a8564b8fdf1d9
Poonam Vilas Jagtap <poonamjagtap0011@gmail.com>
2021-12-10 15:22:07 +0530

    all uploaded

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabotcovidwithomicron-bot-971b-Web-Deploy.pubxml -p:Password=qSzMCdgjprdp8HYerhTDWboQ52r2YKGLfRuvNfFDNZwz3jAyGl7NKqgFqGyD
+
